db02a67da9bbbe60b2fdce3ab4eda55ff31722bf
Svitlana Synytsia <svetlankogr@gmail.com>
2023-09-01 12:17:16 +0200

    open images

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
 import timer from "./modules/timer";
+import images from "./modules/images";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -28,6 +29,6 @@ window.addEventListener("DOMContentLoaded", () => {
     "inline-block"
   );
   forms(modalState);
-
   timer(".container1", deadline);
+  images();
 });
